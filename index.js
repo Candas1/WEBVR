36cd7b02ded85c0ba25d1f77044107be5de4ede9
Candas1 <candascraig@hotmail.com>
2020-04-13 12:10:16 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+var cache = function(object) { 
+
+ alert(object); 
+ document.querySelector('#'+ object).setAttribute("visible",false);
+ 
+}
+
+var montre = function(object) { 
+
+ alert(object); 
+ document.querySelector('#'+ object).setAttribute("visible",true);
+ 
+}
+
+
+if (annyang) {
+	
+  annyang.setLanguage('fr-FR');
+  
+  // Let's define a command.
+  var commands = {
+    'cache :object': cache,
+	'montre :object': montre
+  };
+
+  // Add our commands to annyang
+  annyang.addCommands(commands);
+
+  // Start listening.
+  annyang.start( {continuous: true} );
+}
